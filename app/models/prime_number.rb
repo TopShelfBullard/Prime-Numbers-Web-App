f973ce58cb8cb3a6132aff2dce457574806c4b09
TopShelfBullard <amb@amitchellbullard.com>
2015-03-24 19:39:10 -0500

    Add a method to get the first 1000 prime numbers to the PrimeNumber model.

--- app/models/prime_number.rb
+++ app/models/prime_number.rb
@@ -1,2 +1,8 @@
 class PrimeNumber < ActiveRecord::Base
+  require 'prime'
+
+  def self.get_primes
+    Prime.first(1000)
+  end
+
 end
--- spec/models/prime_number_spec.rb
+++ spec/models/prime_number_spec.rb
@@ -1,5 +1,33 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe PrimeNumber, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe PrimeNumber, :type => :model do
+  it "has a length of 1000" do
+    expect(PrimeNumber.get_primes.length).to eq(1000)
+  end
+
+  it "contains only prime numbers" do
+    results = PrimeNumber.get_primes.map{|number| Prime.prime?(number)}
+    expect(results.include?(false)).to be_falsey
+  end
+
+  it "contains no duplicates" do
+    results = PrimeNumber.get_primes
+    expect(results.length).to eq(results.uniq.length)
+  end
+
+  it "it is sorted smallest to largest" do
+    prior_number = 1
+    PrimeNumber.get_primes.each do |current_number|
+      expect(current_number > prior_number).to be_truthy
+      prior_number = current_number
+    end
+  end
+
+  it "starts with 2" do
+    expect(PrimeNumber.get_primes).to start_with(2)
+  end
+
+  it "ends with 7919" do
+    expect(PrimeNumber.get_primes).to end_with(7919)
+  end
 end
