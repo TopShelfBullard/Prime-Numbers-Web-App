eea7225e765b964cff5888f686644fc031565c8f
TopShelfBullard <amb@amitchellbullard.com>
2015-03-26 02:07:25 -0500

    Fix poor grammar.

--- spec/helpers/prime_numbers_helper_spec.rb
+++ spec/helpers/prime_numbers_helper_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe PrimeNumbersHelper, :type => :helper do
-  it "can product the desired html" do
+  it "can produce the desired html" do
     test_strings = ["first", "second", "third", "fourth"]
     number_groups = helper.primes_in_groups(test_strings, 2)
     expect(number_groups.first).to eq("<span class=\"normal_color\">First, </span><span class=\"offset_color\">Second</span>")
